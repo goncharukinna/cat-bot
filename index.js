d61422637661894871fd6d523ce6f6eaf79f171a
Inna Goncharuk <inna-korrektor@mail.ru>
2026-08-05 23:40:32 +1000

    console.log

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 const { Telegraf } = require('telegraf');
 const https = require('https');
 
+// Лог запуска бота
+console.log('Bot starting...');
+console.log('BOT_TOKEN exists:', !!process.env.BOT_TOKEN);
+
 // Принудительно используем только IPv4
 const agent = new https.Agent({ family: 4 });
 
@@ -17,10 +21,23 @@ const getCatUrl = () =>
   `https://mobimg.b-cdn.net/v3/fetch/d3/d3d2226f52b64f3850a9ab926334d897.jpeg`;
 
 const sendCat = () => {
+  console.log('Sending photo...');
   bot.telegram
     .sendPhoto(chatId, getCatUrl())
-    .then(() => setTimeout(sendCat, intervalMs))
-    .catch((err) => console.error('Error sending photo:', err.message));
+    .then(() => {
+      console.log('Photo sent successfully');
+      setTimeout(sendCat, intervalMs);
+    })
+    .catch((err) => {
+      console.error('Error sending photo:', err.message);
+      setTimeout(sendCat, intervalMs);
+    });
 };
 
+// Обработка неожиданных ошибок (чтобы видеть их в логах)
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught exception:', err);
+});
+
+console.log('Bot is ready, starting send loop...');
 sendCat();
