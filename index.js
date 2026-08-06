b4afe279afa895fe27467b8a7f07459c758f1dba
Inna Goncharuk <inna-korrektor@mail.ru>
2026-08-06 22:14:10 +1000

    Add URL photo

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN, {
 const chatId = '5209781777'; // теперь строка
 const intervalMs = 5000;
 
-const getCatUrl = () =>
-  `https://mobimg.b-cdn.net/v3/fetch/d3/d3d2226f52b64f3850a9ab926334d897.jpeg`;
+const getCatUrl = () => 'https://cataas.com/cat';
 
 // Обработчик команды /hello
 bot.command('hello', (ctx) => {
